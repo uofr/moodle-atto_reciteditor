c05bca2887fd2f32e46391564588bac0a9f40c6b
Gustavo Bazzo <gcbazzo@gmail.com>
2021-05-06 13:35:14 -0400

    Added build

---
+++ src/build/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css"  href="./index.css?v=<?php echo rand(0,100); ?>">
+        <link rel="stylesheet" type="text/css"  href="./bootstrap.min.c9ac70f5.css">
+		<link rel="shortcut icon" type="image/jpg" href="./recit.e0a6c9dd.png"/>
+        <script type="text/javascript" src="./bootstrap.min.c9ac70f5.js"></script>
+    </head>
+    <body>
+        <div id="root"></div>
+        <script type="text/javascript" src="./index.js?v=<?php echo rand(0,100); ?>"></script>
+    </body>
+</html>
