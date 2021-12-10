845ae5d036adb845223dddacba2145e1fb239761
Gustavo Bazzo <gcbazzo@gmail.com>
2021-12-10 11:03:42 -0500

    Update version.php

--- src/version.php
+++ src/version.php
@@ -25,5 +25,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2021092400;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018050800;        // Requires this Moodle version.
 $plugin->component = 'atto_reciteditor';  // Full name of the plugin (used for diagnostics).
-$plugin->release = 'R14-V1.1'; 
-$plugin->maturity = MATURITY_ALPHA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
+$plugin->release = 'R14-V1.2'; 
+$plugin->maturity = MATURITY_BETA; // MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
