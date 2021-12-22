e24d0ddd091ba3b27fba906c9ee60f87df668bb8
Gustavo Bazzo <gcbazzo@gmail.com>
2021-12-22 10:24:32 -0500

    translation

--- src/lang/en/atto_reciteditor.php
+++ src/lang/en/atto_reciteditor.php
@@ -2,4 +2,5 @@
 $string['pluginname'] = 'RÉCIT Editor';
 $string['privacymetadata'] = 'The atto_reciteditor plugin does not store any personal data.';
 $string['htmleditor'] = 'RÉCIT Editor';
+$string['fetch_components'] = 'Fetch Components';
 ?>
--- src/lang/fr/atto_reciteditor.php
+++ src/lang/fr/atto_reciteditor.php
@@ -2,4 +2,5 @@
 $string['pluginname'] = 'Éditeur RÉCIT';
 $string['privacymetadata'] = 'Le plugin atto_reciteditor ne stocke aucune donnée personnelle.';
 $string['htmleditor'] = 'Éditeur RÉCIT';
+$string['fetch_components'] = 'Recherche de composants';
 ?>
